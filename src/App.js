9270ab09ddef1f3d26433735a0f4b5bbb3f1677a
efe-kudos <eguonoghene.efekemo@kudos.com>
2021-04-25 21:53:15 -0600

    Add Material-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles'
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    fontSize: 14,
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      Hello
     </div>
   );
-}
+};
 
 export default App;
